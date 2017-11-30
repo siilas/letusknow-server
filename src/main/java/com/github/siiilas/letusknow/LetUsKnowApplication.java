5cd877e8dd6379736d33291bc8d4f0a9b1cfcd80
Silas Maciel Ferreira <s2it_sferreira@uolinc.com>
2017-11-30 17:46:45 -0200

    Adicionando/configurando Spring MVC no projeto

--- src/main/java/com/github/siiilas/letusknow/LetUsKnowApplication.java
+++ src/main/java/com/github/siiilas/letusknow/LetUsKnowApplication.java
@@ -2,12 +2,19 @@ package com.github.siiilas.letusknow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LetUsKnowApplication {
+public class LetUsKnowApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LetUsKnowApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LetUsKnowApplication.class);
     }
 
 }
--- src/main/java/com/github/siiilas/letusknow/controller/HomeController.java
+++ src/main/java/com/github/siiilas/letusknow/controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.github.siiilas.letusknow.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
-        return "Teste!";
+    public ModelAndView index(Model model) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
     }
 
 }
